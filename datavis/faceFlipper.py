474e7200acc40fe15cda8f50774bef144687ff3f
carykh <carykh@stanford.edu>
2018-12-13 19:43:29 -0800

    moved to folder

--- datavis/faceFlipper.py
+++ datavis/faceFlipper.py
@@ -10,4 +10,4 @@ for i in range(1200):
     image_numpy = misc.imsave('/media/rob/Ma Book1/mugshots/aligned/alignedFace'+str(1200+i)+'.jpg',image_numpy)
     if i%100 == 0:
         print("done with "+str(i))
-print("done af")
+print("all done")
